20c3c0e9b11f13c7bb81c44ac5965885840630e5
Chandan Tiwari <chandan13tiwaris@gmail.com>
2024-01-06 18:17:11 +0530

    updated urls

--- src/main/java/com/poc/portfolio/controller/PortfolioController.java
+++ src/main/java/com/poc/portfolio/controller/PortfolioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
