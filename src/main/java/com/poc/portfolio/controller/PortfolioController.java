c8e25ff6b475eed464fd9a1bfdb8f982f5ff13cb
Chandan Tiwari <chandan13tiwaris@gmail.com>
2024-01-06 16:34:37 +0530

    updated download api thru controller

--- src/main/java/com/poc/portfolio/controller/PortfolioController.java
+++ src/main/java/com/poc/portfolio/controller/PortfolioController.java
@@ -1,14 +1,17 @@
 package com.poc.portfolio.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.File;
@@ -16,6 +19,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Controller
@@ -23,8 +27,7 @@ import java.nio.file.Paths;
 public class PortfolioController {
 
     private static final String RESUME_FILENAME = "chandan-resume.pdf";
-    private static final String RESUME_FILE_PATH = "/src/main/resources/static/file";
-    private static final String PATH_DELIMETER = "/";
+    private static final String RESUME_FILE_PATH = "src/main/resources/static/file/chandan-resume.pdf";
     private static final String RESUME_MEDIA_TYPE = "application/pdf";
 
     @GetMapping("/")
@@ -32,20 +35,17 @@ public class PortfolioController {
         return "index";
     }
 
-    @GetMapping("/download")
-    @ResponseBody
-    public ResponseEntity<Object> download() throws IOException {
-        File resume = new ClassPathResource("static/file" + PATH_DELIMETER + RESUME_FILENAME).getFile();
+    @GetMapping(path = "/download")
+    public ResponseEntity<Resource> download() throws IOException {
+        File file = new File(RESUME_FILE_PATH);
+        Path path = Paths.get(file.getAbsolutePath());
+        ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
 
-        try (FileInputStream fileInputStream = new FileInputStream(resume)) {
-
-            InputStreamResource inputStreamResource = new InputStreamResource(fileInputStream);
-
-           return ResponseEntity.ok()
-                   .contentType(MediaType.parseMediaType(RESUME_MEDIA_TYPE))
-                   .contentLength(Files.size(Paths.get(resume.getPath())))
-                   .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + RESUME_FILENAME + "\"")
-                   .body(inputStreamResource);
-        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + RESUME_FILENAME + "\"")
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType(RESUME_MEDIA_TYPE))
+                .body(resource);
     }
+
 }
